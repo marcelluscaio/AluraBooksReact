8d1524bef894dbd731fb9b63f66201b12e99e003
Caio Cabral <ocaiocabral@gmail.com>
2022-12-12 14:16:37 -0400

    Concluindo Modulo 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
 
-import Logo from './componentes/Logo'
+import Logo from './componentes/Logo';
+import perfil from './imagens/perfil.svg';
+import sacola from './imagens/sacola.svg';
+
+const opcoesMenu = ['Categorias', 'Favoritos', 'Minha estante'];
+const icones = [perfil, sacola]
 
 function App() {
   return (
     <div className='app'>
       <header className='app__header'>
         <Logo></Logo>
+          <ul className={'app__lista'}>
+            {opcoesMenu.map((opcao, indice) => {
+              return <li className={'lista__item'} key={indice}>{opcao}</li>
+              }
+            )}
+          </ul>
+          
+          <ul className={'app__icones'}>
+            {icones.map((icone, indice) => {
+              return <li className={'icones__item'}  key={indice}><img className={'item__imagem'} src={icone} alt={'icone'}></img></li>
+              }
+            )}
+          </ul>
       </header>
     </div>
   );

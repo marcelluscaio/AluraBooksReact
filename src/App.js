839955f89fdb36616a484e1411ada066ea2fd0e5
Caio Cabral <ocaiocabral@gmail.com>
2022-12-14 15:06:11 -0400

    Started using styled components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import './App.css';
-
 import Logo from './componentes/Logo';
 import perfil from './imagens/perfil.svg';
 import sacola from './imagens/sacola.svg';
+import styled from 'styled-components';
 
 const opcoesMenu = ['Categorias', 'Favoritos', 'Minha estante'];
-const icones = [perfil, sacola]
+const icones = [perfil, sacola];
+
+const AppContainer = styled.div`
+    background-image: linear-gradient(180deg ,#fff -25%, #4b0685 250%);
+    height: 100vh;
+    
+  .app__header{
+    background-image: linear-gradient(45deg ,#6d1c04 -25%, #4b0685 150%);
+    padding: 0 5vw;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .app__lista,
+  .app__icones{
+    display: flex;
+    gap: 1em;
+  }
+
+  .lista__item{
+    list-style: none;
+    color: white;
+    cursor: pointer;
+  }
+
+  .icones__item{
+    list-style: none;
+    cursor: pointer;
+    height: 2rem;
+    filter: hue-rotate(180deg) brightness(2.25);
+  }
+`
 
 function App() {
   return (
-    <div className='app'>
+    <AppContainer>
       <header className='app__header'>
         <Logo></Logo>
           <ul className={'app__lista'}>
@@ -26,7 +57,7 @@ function App() {
             )}
           </ul>
       </header>
-    </div>
+    </AppContainer>
   );
 }
 

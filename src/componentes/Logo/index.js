5d011c61a48f0d4a58ec48c85d0c5f0468e3843b
Caio Cabral <ocaiocabral@gmail.com>
2022-12-14 15:24:10 -0400

    End of module 3

--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,12 +1,29 @@
-import './logo.css';
 import logo from '../../imagens/logo.svg';
+import styled from 'styled-components';
+
+const LogoContainer = styled.div`
+      display: flex;
+      align-items: center;
+      font-size: 2rem;
+      gap: 1vw;
+
+   img{
+      height: 2rem;
+   }
+
+   p{
+      color: #FC6721;
+      opacity: 0.85;
+      margin: .5em 0;
+   }
+`
 
 function Logo(){
    return(
-      <div className='logo'>
-          <img src={logo} alt={"Logo da Alura Books"}></img>
+      <LogoContainer>
+          <img src={logo} alt={"Logo da Alura Books"}/>
           <p><b>Alura</b> Books</p>
-      </div>
+      </LogoContainer>
    )
 }
 

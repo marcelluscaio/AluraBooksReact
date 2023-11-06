d0982cb86dd0a3802004a9d4e1911c9aa7ad176c
Caio Cabral <ocaiocabral@gmail.com>
2023-11-06 17:50:29 -0500

    feat: adding Links

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Projeto do curso React Crie sua primeira paagina web da Alura
+
+https://reactrouter.com/en/main/start/tutorial
--- src/App.js
+++ src/App.js
@@ -1,66 +1,85 @@
-import Logo from './componentes/Logo';
-import Pesquisa from './componentes/Pesquisa';
-import perfil from './imagens/perfil.svg';
-import sacola from './imagens/sacola.svg';
-import styled from 'styled-components';
+import Logo from "./componentes/Logo";
+import Pesquisa from "./componentes/Pesquisa";
+import perfil from "./imagens/perfil.svg";
+import sacola from "./imagens/sacola.svg";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-const opcoesMenu = ['Categorias', 'Favoritos', 'Minha estante'];
+const opcoesMenu = ["Categorias", "Favoritos", "Minha estante"];
 const icones = [perfil, sacola];
 
 const AppContainer = styled.div`
-    background-image: linear-gradient(180deg ,#fff -25%, #4b0685 250%);
-    height: 100vh;
-    
-  .app__header{
-    background-image: linear-gradient(45deg ,#6d1c04 -25%, #4b0685 150%);
-    padding: 0 5vw;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
+	background-image: linear-gradient(180deg, #fff -25%, #4b0685 250%);
+	height: 100vh;
 
-  .app__lista,
-  .app__icones{
-    display: flex;
-    gap: 1em;
-  }
+	.app__header {
+		background-image: linear-gradient(45deg, #6d1c04 -25%, #4b0685 150%);
+		padding: 0 5vw;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
 
-  .lista__item{
-    list-style: none;
-    color: white;
-    cursor: pointer;
-  }
+	.app__lista,
+	.app__icones {
+		display: flex;
+		gap: 1em;
+	}
 
-  .icones__item{
-    list-style: none;
-    cursor: pointer;
-    height: 2rem;
-    filter: hue-rotate(180deg) brightness(2.25);
-  }
-`
+	.lista__item {
+		list-style: none;
+		color: white;
+		cursor: pointer;
+	}
+
+	.icones__item {
+		list-style: none;
+		cursor: pointer;
+		height: 2rem;
+		filter: hue-rotate(180deg) brightness(2.25);
+	}
+`;
 
 function App() {
-  return (
-    <AppContainer>
-      <header className='app__header'>
-        <Logo></Logo>
-          <ul className={'app__lista'}>
-            {opcoesMenu.map((opcao, indice) => {
-              return <li className={'lista__item'} key={indice}>{opcao}</li>
-              }
-            )}
-          </ul>
-          
-          <ul className={'app__icones'}>
-            {icones.map((icone, indice) => {
-              return <li className={'icones__item'}  key={indice}><img className={'item__imagem'} src={icone} alt={'icone'}></img></li>
-              }
-            )}
-          </ul>
-      </header>
-      <Pesquisa />
-    </AppContainer>
-  );
+	return (
+		<AppContainer>
+			<header className="app__header">
+				<Logo></Logo>
+				<ul className={"app__lista"}>
+					{opcoesMenu.map((opcao, indice) => {
+						return (
+							<Link to={`/${opcao.toLowerCase().split(" ").join("")}`}>
+								<li
+									className={"lista__item"}
+									key={indice}
+								>
+									{opcao}
+								</li>
+							</Link>
+						);
+					})}
+				</ul>
+
+				<ul className={"app__icones"}>
+					{icones.map((icone, indice) => {
+						return (
+							<li
+								className={"icones__item"}
+								key={indice}
+							>
+								<img
+									className={"item__imagem"}
+									src={icone}
+									alt={"icone"}
+								></img>
+							</li>
+						);
+					})}
+				</ul>
+			</header>
+			<Pesquisa />
+		</AppContainer>
+	);
 }
 
 export default App;
